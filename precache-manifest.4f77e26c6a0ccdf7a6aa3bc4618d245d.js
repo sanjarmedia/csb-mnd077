a4f5be843a1d7242c5a1571fee1e7b5bc3e10c55
Code Sandbox <hello@codesandbox.io>
2023-01-05 17:34:40 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.4f77e26c6a0ccdf7a6aa3bc4618d245d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b837ded546deb48df45290c6b972df64",
+    "url": "https://sanjarmedia.github.io/csb-mnd077/index.html"
+  },
+  {
+    "revision": "5ef6a809dbf73c6971bd",
+    "url": "https://sanjarmedia.github.io/csb-mnd077/static/css/main.52eeca4a.chunk.css"
+  },
+  {
+    "revision": "49fedc958eeed1f17745",
+    "url": "https://sanjarmedia.github.io/csb-mnd077/static/js/2.be81b1b1.chunk.js"
+  },
+  {
+    "revision": "c9b0142727a50cc03f66094cc900509f",
+    "url": "https://sanjarmedia.github.io/csb-mnd077/static/js/2.be81b1b1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5ef6a809dbf73c6971bd",
+    "url": "https://sanjarmedia.github.io/csb-mnd077/static/js/main.0c0336b3.chunk.js"
+  },
+  {
+    "revision": "aed843bd7d991375001e",
+    "url": "https://sanjarmedia.github.io/csb-mnd077/static/js/runtime-main.1f38cc95.js"
+  },
+  {
+    "revision": "16bcbe8df3b9d5dbf556397864c80153",
+    "url": "https://sanjarmedia.github.io/csb-mnd077/static/media/logo.16bcbe8d.png"
+  }
+]);
